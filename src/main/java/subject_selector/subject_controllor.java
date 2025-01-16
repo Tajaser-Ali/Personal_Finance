c95e783efe04cf49d80bd1386cd0792fcb50d14e
Tajaser-Ali <tajaser.ali@thekaustschool.org>
2025-01-16 15:10:02 +0300

    Test1 added

--- src/main/java/subject_selector/subject_controllor.java
+++ src/main/java/subject_selector/subject_controllor.java
@@ -1,8 +1,11 @@
 package subject_selector;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 
 import javax.swing.*;
@@ -43,12 +46,40 @@ public class subject_controllor {
     Label group6Lbl;
 
     @FXML
+    Label lblLang;
+
+    @FXML
     TextArea txtArea1;
 
-    public void onClickAddEntry(ActionEvent actionEvent) {
+    public void initialize(){
+        MenuButton mbLang = new MenuButton("Language");
+
+        // create menuitems
+        MenuItem m1 = new MenuItem("French");
+        MenuItem m2 = new MenuItem("Spanish");
+        MenuItem m3 = new MenuItem("Arabic A");
+
+        // add menu items to menu
+        mbLang.getItems().add(m1);
+        mbLang.getItems().add(m2);
+        mbLang.getItems().add(m3);
+
+        EventHandler<ActionEvent> event1 = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent e)
+            {
+                lblLang.setText(((MenuItem)e.getSource()).getText());
+                System.out.println(lblLang.getText());
+            }
+        };
+        m1.setOnAction(test1);
+        m2.setOnAction(event1);
+        m3.setOnAction(event1);
     }
 
-    public static void main(String[] args) {
+    public void test1(){
+        lblLang.setText("French");
+    }
 
+    public void onClickAddEntry(ActionEvent actionEvent) {
     }
 }
--- src/main/java/subject_selector/subjects.java
+++ src/main/java/subject_selector/subjects.java
@@ -1,5 +1,43 @@
 package subject_selector;
 
 public class subjects {
-    
+    //attributes
+    private String group;
+    private String level;
+    private String subjectName;
+
+    public subjects(String subjectName, String level, String group) {
+        this.subjectName = subjectName;
+        this.level = level;
+        this.group = group;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    @Override
+    public String toString() {
+        return group + " " + subjectName + " " + level + "\n";
+    }
 }
